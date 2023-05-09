7c77a2c185e1af7c0b17683ece99fa7ee2feef23
ore <go1ruf2tk3@gmail.com>
2023-05-09 18:46:15 +0900

    [#9] feat: 라우터 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
@@ -10,54 +10,66 @@ import TeacherDetail from "./pages/TeacherDetail";
 import StudentDetail from "./pages/StudentDetail";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
+import NotFound from "./pages/NotFound";
 
 function App() {
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/signup",
-      element: <SignUp />,
-    },
-    {
-      path: "/signin",
-      element: <SignIn />,
-    },
-    {
-      path: "/user/:userId",
-      element: <User />,
-    },
-    {
-      path: "/teachers",
-      element: <Teachers />,
-    },
-    {
-      path: "/students",
-      element: <Students />,
-    },
-    {
-      path: "/notice",
-      element: <Notice />,
-    },
-    {
-      path: "/teachers/:postId",
-      element: <TeacherDetail />,
-    },
-    {
-      path: "/students/:postId",
-      element: <StudentDetail />,
+      element: <Layout />,
+      errorElement: <NotFound />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/signup",
+          element: <SignUp />,
+        },
+        {
+          path: "/signin",
+          element: <SignIn />,
+        },
+        {
+          path: "/user/:userId",
+          element: <User />,
+        },
+        {
+          path: "/teachers",
+          element: <Teachers />,
+        },
+        {
+          path: "/students",
+          element: <Students />,
+        },
+        {
+          path: "/notice",
+          element: <Notice />,
+        },
+        {
+          path: "/teachers/:postId",
+          element: <TeacherDetail />,
+        },
+        {
+          path: "/students/:postId",
+          element: <StudentDetail />,
+        },
+      ],
     },
   ]);
 
-  return (
-    <>
-      <Header />
-      <RouterProvider router={router} />
-      <Footer />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
