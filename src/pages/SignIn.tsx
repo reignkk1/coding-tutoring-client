a0b6b11727c27174a83fc7bf9d9387934d5bfbdf
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2023-05-11 18:09:18 +0900

    Revert "refactor: 로직 컴포넌트"
    This reverts commit 2090eb5350364d64991db5506a2735d903b45867.

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,20 +1,60 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 import Wrapper from "../components/common/Wrapper";
-import useForm from "../hooks/useForm";
-import { Form, FormContainer } from "../styles/Form";
+
+interface Inputs {
+  email: string;
+  password: string;
+}
+
+interface Valid {
+  email: boolean;
+  password: boolean;
+}
 
 export default function SignIn(): JSX.Element {
-  const {
-    submitHandler,
-    changeHandler,
-    validHandler,
-    inputs,
-    valid,
-    formIsValid,
-  } = useForm({
+  const [inputs, setInputs] = useState<Inputs>({
     email: "",
     password: "",
   });
+
+  const [valid, setValid] = useState<Valid>({
+    email: false,
+    password: false,
+  });
+
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      console.log("유효성 검사중");
+      setFormIsValid(inputs.email.includes("@") && inputs.password.length >= 8);
+    }, 500);
+
+    return () => {
+      console.log("클린업");
+      clearTimeout(identifier);
+    };
+  }, [inputs.email, inputs.password]);
+
+  const changeHandler = (e: React.FormEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target as HTMLInputElement;
+    setInputs({ ...inputs, [name]: value });
+  };
+
+  const validHandler = (e: React.FormEvent<HTMLInputElement>): void => {
+    const { name } = e.target as HTMLInputElement;
+    if (name === "email") {
+      setValid({ ...valid, [name]: inputs.email.includes("@") });
+    } else {
+      setValid({ ...valid, [name]: inputs.password.length >= 8 });
+    }
+  };
+
+  const submitHandler = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+  };
 
   return (
     <Wrapper>
@@ -61,3 +101,87 @@ export default function SignIn(): JSX.Element {
     </Wrapper>
   );
 }
+
+export const Form = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #697a79;
+
+  h3 {
+    font-size: 2rem;
+    font-weight: 600;
+  }
+
+  .social {
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
+    margin-top: 2rem;
+  }
+
+  p {
+    margin-top: 2rem;
+  }
+
+  .warn {
+    margin-top: 0.5rem;
+    color: #de3730ff;
+    font-size: 0.8rem;
+  }
+
+  input {
+    width: 250px;
+    background-color: #f0f4f3;
+    border: none;
+    margin-top: 1rem;
+    padding: 0.8rem 1rem;
+    color: #697a79;
+
+    &::placeholder {
+      opacity: 0.8;
+    }
+  }
+
+  button {
+    border-radius: 1.2rem;
+    border: none;
+    background-color: #3ab19b;
+    color: #ffffff;
+    padding: 0.8rem 3rem;
+    margin-top: 1.5rem;
+    transition: transform 0.1s ease-in;
+
+    &.disabled {
+      background-color: #de3730ff;
+    }
+
+    &:hover {
+      transform: scale(0.95);
+    }
+  }
+`;
+
+export const FormContainer = styled.div`
+  max-width: 400px;
+  margin-inline: auto;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .aboutSign {
+    display: flex;
+    gap: 0.5rem;
+    color: #697a79;
+    margin-top: 1.5rem;
+    font-size: 0.8rem;
+
+    a {
+      color: inherit;
+      text-decoration: none;
+    }
+  }
+`;
