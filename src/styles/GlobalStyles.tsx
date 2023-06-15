959ba7db31d8c1d0f4ac8e37f42b6cb81015d346
ore <go1ruf2tk3@gmail.com>
2023-06-15 15:11:10 +0900

    style: Jamsil 폰트 추가

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -22,6 +22,46 @@ ${reset}
     box-sizing:border-box;
 }
 
+@font-face {
+    font-family: 'thin';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2302_01@1.0/TheJamsil1Thin.woff2') format('woff2');
+    font-weight: 100;
+    font-style: normal;
+}
+@font-face {
+    font-family: 'light';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2302_01@1.0/TheJamsil2Light.woff2') format('woff2');
+    font-weight: 300;
+    font-style: normal;
+}
+@font-face {
+    font-family: 'regular';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2302_01@1.0/TheJamsil3Regular.woff2') format('woff2');
+    font-weight: 400;
+    font-style: normal;
+}
+@font-face {
+    font-family: 'medium';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2302_01@1.0/TheJamsil4Medium.woff2') format('woff2');
+    font-weight: 500;
+    font-style: normal;
+}
+@font-face {
+    font-family: 'bold';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2302_01@1.0/TheJamsil5Bold.woff2') format('woff2');
+    font-weight: 700;
+    font-style: normal;
+}
+
+
+
+body{
+    font-family: light;
+    background-color: #0e1620;
+    color: #ffffff;
+}
+
+
 a {
     text-decoration:none;
 }
